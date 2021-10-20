0fcfa1bb139e5c28c97ba84626e688f400340e4e
Andrei Delesevich <delesevich@gmail.com>
2021-10-20 14:27:29 +0300

    Initial commit

---
+++ src/test/java/by/delesevich/car_marketplace/CarMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.delesevich.car_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMarketplaceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
