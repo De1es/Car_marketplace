56b2ee3b75eb2ed7441c95aebf1141cd988a3687
Andrei Delesevich <delesevich@gmail.com>
2021-10-24 18:24:31 +0300

    To LiquiBase changeset added missed field in users table (date_of_delete)

--- src/main/java/by/delesevich/car_marketplace/entity/User.java
+++ src/main/java/by/delesevich/car_marketplace/entity/User.java
@@ -3,6 +3,7 @@ package by.delesevich.car_marketplace.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 import java.util.Set;
 
 @Entity
@@ -11,17 +12,20 @@ import java.util.Set;
 public class User {
 
   @Id
-  @GeneratedValue (strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "u_id")
   private Long id;
 
-  @Column (name = "u_login")
+  @Column(name = "u_login")
   private String login;
 
-  @Column (name = "u_password")
+  @Column(name = "u_password")
   private String password;
 
-  @Column (name = "u_role")
+  @Column(name = "u_role")
   private String role;
+
+  @Column(name = "u_date_of_delete")
+  private Timestamp dateOfDelete;
 
 }
